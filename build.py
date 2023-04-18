018bec560f12a3b1fced2a7f7b438d7ae423bc13
Brad Miller <bonelake@mac.com>
2023-04-18 11:16:59 -0500

    New: build.py
    * build all wheels
    * build all images
    * check environment

---
+++ build.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+import os
+import subprocess
+
+print("Checking your environment")
+finish = False
+for var in [
+    "RUNESTONE_PATH",
+    "SERVER_CONFIG",
+    "DBURL",
+    "DEV_DBURL",
+    "BOOK_PATH",
+    "WEB2PY_CONFIG",
+]:
+    if var not in os.environ:
+        print(f"Environment variable {var} not set")
+        finish = True
+    else:
+        print(f"{var} is set")
+
+if finish:
+    print(
+        "Your environment is not set up correctly.  Please define the environment variables listed above."
+    )
+    exit(1)
+
+
+print("Building projects")
+for proj in os.listdir("projects"):
+    if os.path.isdir(proj):
+        os.chdir(f"projects/{proj}")
+        print(f"Project: {proj}")
+        if os.path.isfile("pyproject.toml"):
+            subprocess.run(["poetry", "build-project"])
+        os.chdir("../..")
+
+print("Building docker images")
+subprocess.run(["docker", "compose", "build"])
