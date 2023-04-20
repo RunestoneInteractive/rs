5fef74ad8039c28e6880e1a6f60410176fb0f3ca
Brad Miller <bonelake@mac.com>
2023-04-20 08:40:05 -0500

    New: copy 1.py.prototype as needed

--- build.py
+++ build.py
@@ -11,6 +11,7 @@ print("Checking your environment")
 finish = False
 for var in [
     "RUNESTONE_PATH",
+    "RUNESTONE_HOST",
     "SERVER_CONFIG",
     "DBURL",
     "DEV_DBURL",
@@ -22,6 +23,18 @@ for var in [
         finish = True
     else:
         print(f"{var} is set")
+
+if not os.path.isfile("bases/rsptx/web2py_server/applications/runestone/models/1.py"):
+    # copy 1.py.prototype to 1.py
+    print("Copying 1.py.prototype to 1.py")
+    subprocess.run(
+        [
+            "cp",
+            "bases/rsptx/web2py_server/applications/runestone/models/1.py.prototype",
+            "bases/rsptx/web2py_server/applications/runestone/models/1.py",
+        ]
+    )
+
 
 if finish:
     print(
