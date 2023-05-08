221a595cb177373b94fbece87bcb2ed77694c577
Brad Miller <bonelake@mac.com>
2023-05-08 14:56:18 -0700

    Remove old docker config stuff to reduce confusion

--- bases/rsptx/web2py_server/applications/runestone/docker/gunicorn_config/__init__.py
+++
@@ -1,4 +0,0 @@
-# *********
-# |docname|
-# *********
-# This is required so that imports of ``gunicorn_config.common_config`` work. (Naming the subdirectory ``gunicorn`` conflicts with the ``gunicorn`` module, of course.)
