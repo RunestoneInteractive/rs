30ac989567d66fbb92a622a4d07be48f337ce6d0
Brad Miller <bonelake@mac.com>
2023-05-26 07:48:47 -0500

    Add: option to publish

--- projects/interactives/build.py
+++ projects/interactives/build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import subprocess
+import sys
 from rsptx.cl_utils.core import pushd
 import toml
 
@@ -14,3 +15,39 @@ with pushd("../../bases/rsptx/interactives"):
 
 
 subprocess.run(["poetry", "build-project"], check=True)
+
+
+if sys.argv[1:] == ["--publish"]:
+    # subprocess.run(["poetry", "publish"], check=True)
+
+    with pushd("../../bases/rsptx/interactives/runestone"):
+        subprocess.run(
+            " ".join(
+                [
+                    "tar",
+                    "--strip-components",
+                    "1",
+                    "-zcf",
+                    f"dist-{VERSION}.tgz",
+                    "dist/*",
+                ]
+            ),
+            check=True,
+            shell=True,  # needed so that the glob is expanded
+        )
+        print("Installing release on CDN")
+        subprocess.run(
+            ["scp", f"dist-{VERSION}.tgz", "balance.runestoneacademy.org:~/"],
+            check=True, 
+        )
+        subprocess.run(
+            [
+                "ssh",
+                "balance.runestoneacademy.org",
+                "/home/bmiller/bin/install_release.sh",
+                VERSION,
+            ],
+            check=True,
+        )
+        print("Moving release to jsdist")
+        subprocess.run(["mv", f"dist-{VERSION}.tgz", "../jsdist"], check=True)
