17bbc030e8ef2d0c5be40fc89ec96d05d112ef2c
bjones1 <bjones@ece.msstate.edu>
2023-06-21 14:43:03 -0500

    Fix: Move to a Windows-friendly subprocess approach.

--- build.py
+++ build.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
-
-import os
+#
+# `build.py` - Build the wheels and Docker containers needed for this application
+# ===============================================================================
+#
+# Imports
+# -------
+import asyncio
+import io
 import locale
-from shutil import copyfile
+import os
 import subprocess
 import sys
+from shutil import copyfile
+from typing import Any
 
 # use python-dotenv >= 0.21.0
 from dotenv import load_dotenv
 from rich.console import Console
-from rich.table import Table
 from rich.live import Live
+from rich.table import Table
 from rsptx.cl_utils.core import pushd
-import sh
 
+# Check environment variables
+# ---------------------------
 print("Checking your environment")
 if not os.path.exists(".env"):
     print("No .env file found.  Please copy sample.env to .env and edit it.")
@@ -24,6 +33,7 @@ if "--verbose" in sys.argv:
 else:
     VERBOSE = False
 
+# Per the [docs](https://pypi.org/project/python-dotenv/), load `.env` into environment variables.
 load_dotenv()
 console = Console()
 table = Table(title="Environment Variables")
@@ -67,13 +77,11 @@ else:
     print("DC_DEV_DBURL set.  Using it instead of DEV_DBURL")
 
 if not os.path.isfile("bases/rsptx/web2py_server/applications/runestone/models/1.py"):
-    # copy 1.py.prototype to 1.py
     print("Copying 1.py.prototype to 1.py")
     copyfile(
         "bases/rsptx/web2py_server/applications/runestone/models/1.py.prototype",
         "bases/rsptx/web2py_server/applications/runestone/models/1.py",
     )
-
 
 if finish:
     print(
@@ -92,10 +100,14 @@ if finish:
 #           return codecs.charmap_encode(input,self.errors,encoding_table)[0]
 #       UnicodeEncodeError: 'charmap' codec can't encode characters in position 55319-55358: character maps to <undefined>
 try:
-    stdout_err_encoding = locale.getencoding()
+    # Since this isn't defined on all Python versions, tell mypy to ignore the following error.
+    stdout_err_encoding = locale.getencoding()  # type: ignore[attr-defined]
 except AttributeError:
     stdout_err_encoding = locale.getpreferredencoding()
 
+
+# Build wheels
+# ------------
 table = Table(title="Build Wheels")
 table.add_column("Project", justify="right", style="cyan", no_wrap=True)
 table.add_column("Built", style="magenta")
@@ -118,35 +130,60 @@ with Live(table, refresh_per_second=4):
                                 f.write(res.stderr.decode(stdout_err_encoding))
 
 
-# When using subprocess there is no nice way to try (short of threads) to capture the output and send
-# it to the console while also capturing it to a file.  So we use sh instead.
-# https://amoffat.github.io/sh/index.html
-# sh "knows" about all of the commands on your PATH
+# Build Docker containers
+# -----------------------
+# The following code is based on a post on [SO](https://stackoverflow.com/a/66400096/16038919).
+## {
+# Given an input and output(s), copy from one to the other(s).
+async def output_stream(input_stream: asyncio.StreamReader, *output_stream: io.IOBase):
+    while not input_stream.at_eof():
+        output = await input_stream.readline()
+        for _os in output_stream:
+            _os.write(output)
+            _os.flush()
 
-# define a function that will take the output from the background process
-# if it matches a pattern we are looking for, print it to the console
-def process_output(line, stdin, process):
-    if "naming to docker.io" in line and "done" in line:
-        print(line[line.rindex("/") + 1 :].replace("done", "").strip())
+
+# Run a subprocess and stream the output while it runs. Return the process' return code.
+async def run_command(*command: str, **kwargs: Any) -> int:
+    # Start the subprocess.
+    process = await asyncio.create_subprocess_exec(
+        *command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        **kwargs,
+    )
+    # Since we passed `PIPE` for these, they should be available.
+    assert process.stderr is not None
+    assert process.stdout is not None
+
+    # Stream stdout and stderr while it runs. Save all stdout to a file.
+    with open("build.log", "ab") as f:
+        # Note that the asyncio subprocess `stderr` and `stdout` produces bytes only, not text. So, we need to write to `sys.stdout/err` as binary. Therefore, use [.buffer](https://docs.python.org/3/library/io.html#io.TextIOBase.buffer).
+        await asyncio.gather(
+            output_stream(process.stderr, sys.stderr.buffer),
+            output_stream(process.stdout, sys.stdout.buffer, f),
+        )
+
+    # Terminate it nicely.
+    #
+    # `process.communicate()` will have no data to read but will close the
+    # pipes that are implemented in C, whereas `process.wait()`` will not.
+    stdout_data, stderr_data = await process.communicate()
+    assert not stdout_data and not stderr_data
+
+    assert process.returncode is not None
+    return process.returncode
+
+
+## }
 
 
 print("Building docker images...")
-# res = subprocess.run(["docker", "compose", "build"], capture_output=True)
-
-try:
-    # such a cool way to run "docker compose build" and capture the output
-    res = sh.docker.compose.build(
-        "--progress", "plain", _out=process_output, _bg=True, _tee=True
-    )
-    res.wait()
+ret = asyncio.run(run_command("docker", "compose", "build"))
+if ret == 0:
     print("Docker images built successfully")
-    with open("build.log", "a") as f:
-        f.write(res.stdout.decode(stdout_err_encoding))
-except sh.ErrorReturnCode:
+else:
     print(
         "Docker images failed to build, see build.log for details (or run with --verbose)"
     )
-    print(res.stderr.decode(stdout_err_encoding))
-    with open("build.log", "a") as f:
-        f.write(res.stderr.decode(stdout_err_encoding))
     exit(1)
