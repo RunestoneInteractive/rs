92e182d06106f548f7e9dcf8bb17fd0e40f77e8b
Bradley Miller <bonelake@mac.com>
2023-08-05 16:16:07 -0500

    use the context manager

--- bases/rsptx/book_server_api/localconfig.py
+++ bases/rsptx/book_server_api/localconfig.py
@@ -3,12 +3,13 @@ from pathlib import Path
 import importlib.resources
 from pydantic import BaseSettings
 
+with importlib.resources.path("rsptx.book_server_api", "") as path:
+    bs_path = Path(path)
+
 
 class LocalConfig(BaseSettings):
     # Provide a path to the book server files. The leading underscore prevents environment variables from affecting this value. See the `docs <https://pydantic-docs.helpmanual.io/usage/models/#automatically-excluded-attributes>`_, which don't say this explicitly, but testing confirms it.
-    _book_server_path: str = str(
-        Path(importlib.resources.path("rsptx.book_server_api", "")).absolute()
-    )
+    _book_server_path: str = str(Path(bs_path).absolute())
 
 
 local_settings = LocalConfig()
