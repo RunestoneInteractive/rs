438b6361eda67cae80a8f5939addac2b58079e0c
Bradley Miller <bonelake@mac.com>
2023-09-02 11:00:44 -0500

    Fix: too many nots

--- bases/rsptx/web2py_server/applications/runestone/controllers/admin.py
+++ bases/rsptx/web2py_server/applications/runestone/controllers/admin.py
@@ -715,7 +715,7 @@ def course_students():
 
     searchdict = OrderedDict()
     for row in cur_students:
-        if not row.id not in iset:
+        if row.id not in iset:
             name = row.first_name + " " + row.last_name
             username = row.username
             searchdict[str(username)] = name
@@ -740,7 +740,6 @@ def grading():
     for i in instructors:
         iset.add(i.instructor)
 
-    
     for row in assignments_query:
         assignmentids[row.name] = int(row.id)
         # Retrieve relevant info for each question, ordering them based on their
