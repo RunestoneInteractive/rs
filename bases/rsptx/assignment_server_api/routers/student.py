0c9203a556ab511af20155943feeabb39ddeba34
Bradley Miller <bonelake@mac.com>
2023-09-05 16:49:52 -0500

    Fix: Get the right key for latex_macros!!

--- bases/rsptx/assignment_server_api/routers/student.py
+++ bases/rsptx/assignment_server_api/routers/student.py
@@ -407,7 +407,7 @@ async def doAssignment(
         ptx_js_version=course_attrs.get("ptx_js_version", "0.2"),
         webwork_js_version=course_attrs.get("webwork_js_version", "2.17"),
         request=request,
-        latex_preamble=course_attrs.get("latex_preamble", ""),
+        latex_preamble=course_attrs.get("latex_macros", ""),
         wp_imports=get_webpack_static_imports(course),
         settings=settings,
     )
