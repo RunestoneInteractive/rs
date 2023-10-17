9fabaf3dfe162db0f90ba9d18d7bd960d38324e2
Bradley Miller <bonelake@mac.com>
2023-10-17 17:25:27 -0500

    make sure errors folder is there

--- projects/w2p_login_assign_grade/entrypoint.sh
+++ projects/w2p_login_assign_grade/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /usr/local/lib/python3.10/site-packages/rsptx/web2py_server
+mkdir -p applications/runestone/errors
 python web2py.py -S runestone -M -R tickets2db.py &
 cd /usr/src/app
 gunicorn --bind 0.0.0.0:8112 rsptx.web2py_server.wsgihandler:application
