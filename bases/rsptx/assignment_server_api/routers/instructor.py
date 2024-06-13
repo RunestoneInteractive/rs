afa754abca64b35cb47e66999c2d583c669e8b6a
Bradley Miller <bonelake@mac.com>
2024-06-13 11:42:22 -0500

    New: minimal template framework for using react
    * provide a real eBookConfig object
    * provide course attributes
    * load runestone webpack from the current book

--- bases/rsptx/assignment_server_api/routers/instructor.py
+++ bases/rsptx/assignment_server_api/routers/instructor.py
@@ -17,6 +17,7 @@ from rsptx.db.crud import (
     fetch_assignments,
     fetch_questions_by_search_criteria,
     fetch_question_count_per_subchapter,
+    fetch_all_course_attributes,
     create_assignment_question,
     create_question,
     fetch_course,
@@ -30,7 +31,11 @@ from rsptx.db.crud import (
 from rsptx.auth.session import auth_manager, is_instructor
 from rsptx.templates import template_folder
 from rsptx.configuration import settings
-from rsptx.response_helpers.core import make_json_response
+from rsptx.response_helpers.core import (
+    make_json_response,
+    get_webpack_static_imports,
+    get_react_imports,
+)
 from rsptx.db.models import (
     AssignmentQuestionValidator,
     AssignmentValidator,
@@ -515,9 +520,31 @@ async def get_builder(
         return RedirectResponse(url="/")
 
     reactdir = pathlib.Path(__file__).parent.parent / "react"
-    with open(reactdir / "index.html") as f:
-        content = f.read()
-    return HTMLResponse(content=content)
+    templates = Jinja2Templates(directory=template_folder)
+    wp_imports = get_webpack_static_imports(course)
+    react_imports = get_react_imports(reactdir)
+    course_attrs = await fetch_all_course_attributes(course.id)
+
+    return templates.TemplateResponse(
+        "assignment/instructor/builder.html",
+        {
+            "course": course,
+            "user": user.username,
+            "request": request,
+            "is_instructor": user_is_instructor,
+            "student_page": False,
+            "wp_imports": wp_imports,
+            "react_imports": react_imports,
+            "settings": settings,
+            "latex_preamble": course_attrs.get("latex_macros", ""),
+            "ptx_js_version": course_attrs.get("ptx_js_version", "0.2"),
+            "webwork_js_version": course_attrs.get("webwork_js_version", "2.17"),
+        },
+    )
+
+    # with open(reactdir / "index.html") as f:
+    #     content = f.read()
+    # return HTMLResponse(content=content)
 
 
 @router.get("/grader")
