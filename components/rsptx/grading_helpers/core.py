38c1852a07652c660f1e5f1ad02d717ef2a48440
Bradley Miller <bonelake@mac.com>
2024-07-01 15:38:24 -0500

    New: score on submit
    * If problem is part of an assignment then update the score
    * Move book server to python 3.11

--- components/rsptx/grading_helpers/core.py
+++ components/rsptx/grading_helpers/core.py
@@ -1,9 +1,23 @@
+from typing import Union
 from rsptx.db.models import AuthUserValidator
-from rsptx.db.crud import is_assigned, fetch_answers, create_question_grade_entry, fetch_question_grade, update_question_grade_entry, upsert_grade, fetch_assignment_scores
+from rsptx.db.crud import (
+    is_assigned,
+    fetch_answers,
+    create_question_grade_entry,
+    fetch_grade,
+    fetch_question_grade,
+    update_question_grade_entry,
+    upsert_grade,
+    fetch_assignment_scores,
+)
 from rsptx.validation.schemas import LogItemIncoming, ScoringSpecification
 from rsptx.db.models import GradeValidator
+from rsptx.logging import rslogger
 
-async def grade_submission(user: AuthUserValidator, submission: LogItemIncoming) -> None:
+
+async def grade_submission(
+    user: AuthUserValidator, submission: LogItemIncoming
+) -> None:
     """
     Grade a submission and store the results in the database.
 
@@ -12,49 +26,96 @@ async def grade_submission(user: AuthUserValidator, submission: LogItemIncoming)
     :param submission: The submission to grade.
     :type submission: LogItemIncoming
     """
-    
+
     # First figure out if the answer is part of an assignment
+    update_total = False
     scoreSpec = await is_assigned(submission.div_id, user.course_id)
     if scoreSpec.assigned:
+        rslogger.debug(
+            f"Scoring {submission.div_id} for {user.username} scoreSpec = {scoreSpec}"
+        )
         if scoreSpec.which_to_grade == "first_answer":
-            answers = await fetch_answers(submission.div_id,submission.event, user.course_name, user.username)
+            answers = await fetch_answers(
+                submission.div_id, submission.event, user.course_name, user.username
+            )
             if len(answers) > 0:
                 return
             else:
-                scoreSpec.score = score_one_answer(scoreSpec, submission=)
-                await create_question_grade_entry(user.username, user.course_name, submission.div_id, scoreSpec.score)
+                scoreSpec.score = score_one_answer(scoreSpec, submission)
+                await create_question_grade_entry(
+                    user.username, user.course_name, submission.div_id, scoreSpec.score
+                )
+                update_total = True
         elif scoreSpec.which_to_grade == "last_answer":
             scoreSpec.score = score_one_answer(scoreSpec, submission)
-            answer = await fetch_question_grade(user.username, user.course_name, submission.div_id)
+            answer = await fetch_question_grade(
+                user.username, user.course_name, submission.div_id
+            )
             if answer:
                 answer.score = scoreSpec.score
-                await update_question_grade_entry(user.username, user.course_name, submission.div_id, scoreSpec.score)
+                await update_question_grade_entry(
+                    user.username, user.course_name, submission.div_id, scoreSpec.score
+                )
             else:
-                await create_question_grade_entry(user.username, user.course_name, submission.div_id, scoreSpec.score)
+                await create_question_grade_entry(
+                    user.username, user.course_name, submission.div_id, scoreSpec.score
+                )
+            update_total = True
         elif scoreSpec.which_to_grade == "best_answer":
             scoreSpec.score = score_one_answer(scoreSpec, submission)
-            current_score = await fetch_question_grade(user.username, user.course_name, submission.div_id)
-            if current_score < scoreSpec.score:
-                await update_question_grade_entry(user.username, user.course_name, submission.div_id, scoreSpec.score)
+            rslogger.debug(
+                f"Scoring best answer with current score of {scoreSpec.score}"
+            )
+            current_score = await fetch_question_grade(
+                user.username, user.course_name, submission.div_id
+            )
+            if current_score:
+                if current_score.score < scoreSpec.score:
+                    await update_question_grade_entry(
+                        user.username,
+                        user.course_name,
+                        submission.div_id,
+                        scoreSpec.score,
+                    )
+                    update_total = True
+            else:
+                await create_question_grade_entry(
+                    user.username,
+                    user.course_name,
+                    submission.div_id,
+                    scoreSpec.score,
+                )
+                update_total = True
+        if update_total:
+            rslogger.debug("Updating total score")
+            # Now compute the total
+            res = await fetch_assignment_scores(
+                scoreSpec.assignment_id, user.course_name, user.username
+            )
+            total = 0
+            for row in res:
+                total += row.score
 
-        # Now compute the total
-        res = await fetch_assignment_scores(scoreSpec.assignment_id, user.course_name, user.username)
-        total = 0
-        for row in res:
-            total += row.score
+            # Now update the grade table with the new total
 
-        # Now update the grade table with the new total
+            grade = await fetch_grade(user.id, scoreSpec.assignment_id)
+            if grade:
+                grade.score = total
+                newGrade = grade
+            else:
+                newGrade = GradeValidator(
+                    auth_user=user.id,
+                    course_name=user.course_name,
+                    assignment=scoreSpec.assignment_id,
+                    score=total,
+                    manual_total=False,
+                )
+            res = await upsert_grade(newGrade)
 
-        newGrade = GradeValidator(
-            auth_user=user.id,
-            course_name=user.course_name,
-            assignment=scoreSpec.assignment_id,
-            score=total,
-        )
-        upsert_grade(newGrade)
 
-
-def score_one_answer(scoreSpec: ScoringSpecification, submission: LogItemIncoming) -> Union[int, float]:
+def score_one_answer(
+    scoreSpec: ScoringSpecification, submission: LogItemIncoming
+) -> Union[int, float]:
     """
     Score a single answer based on the scoring specification.
 
@@ -64,12 +125,12 @@ def score_one_answer(scoreSpec: ScoringSpecification, submission: LogItemIncomin
     :rtype: Union[int, float]
     """
     if scoreSpec.how_to_score == "pct_correct":
-        if scoreSpec.correct:
+        if submission.correct:
             return scoreSpec.max_score
         else:
-            return LogItemIncoming.percentage * scoreSpec.max_score
+            return submission.percentage * scoreSpec.max_score
     elif scoreSpec.how_to_score == "all_or_nothing":
-        if scoreSpec.correct:
+        if submission.correct:
             return scoreSpec.max_score
         else:
             return 0
