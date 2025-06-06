3073c3a5cfb5fb8dcf8aaeedc12756f11d490adf
Bradley Miller <bonelake@mac.com>
2025-06-06 17:12:27 -0500

    Add book.py

--- components/rsptx/db/crud/__init__.py
+++ components/rsptx/db/crud/__init__.py
@@ -10,21 +10,55 @@ from .course import (
     fetch_courses_for_user,
     fetch_users_for_course,
     user_in_course,
-
+)
+from .book import (
+    get_book_chapters,
+    fetch_chapter_for_subchapter,
+    get_book_subchapters,
+    fetch_page_activity_counts,
+    create_user_state_entry,
+    update_user_state,
+    update_sub_chapter_progress,
+    fetch_last_page,
+    fetch_user_sub_chapter_progress,
+    create_user_sub_chapter_progress_entry,
+    fetch_user_chapter_progress,
+    create_user_chapter_progress_entry,
 )
 
+# from .user
 __all__ = [
     "fetch_user",
     "create_user",
     "update_user",
     "delete_user",
-    "fetch_course",
+]
+
+# from .course
+__all__ += [
     "create_course",
-    "fetch_course_by_id",
-    "fetch_courses_for_user",
-    "fetch_base_course",
-    "user_in_course",
-    "fetch_users_for_course",
     "create_user_course_entry",
     "delete_user_course_entry",
+    "fetch_base_course",
+    "fetch_course_by_id",
+    "fetch_course",
+    "fetch_courses_for_user",
+    "fetch_users_for_course",
+    "user_in_course",
+]
+
+# from .book
+__all__ += [
+    "create_user_chapter_progress_entry",
+    "create_user_state_entry",
+    "create_user_sub_chapter_progress_entry",
+    "fetch_chapter_for_subchapter",
+    "fetch_last_page",
+    "fetch_page_activity_counts",
+    "fetch_user_chapter_progress",
+    "fetch_user_sub_chapter_progress",
+    "get_book_chapters",
+    "get_book_subchapters",
+    "update_sub_chapter_progress",
+    "update_user_state",
 ]
