a31ec8c828d6cbd153bc9bda6ce8c10b2d3d14a9
andreimarozau <mauk.lekph@gmail.com>
2025-12-02 23:33:32 +0300

    Revert "issue-997 Initial implementation of clickable area"
    This reverts commit 999eb7d8357305dc523d5cbbeac7aa52f0338f08.

--- bases/rsptx/assignment_server_api/assignment_builder/src/components/routes/AssignmentBuilder/components/exercises/components/CreateExercise/components/ClickableAreaExercise/index.ts
+++
@@ -1,6 +0,0 @@
-export { ClickableAreaExercise } from "./ClickableAreaExercise";
-export { ClickableAreaEditor } from "./ClickableAreaEditor";
-export { ClickableAreaList } from "./ClickableAreaList";
-export { ClickableAreaPreview } from "./ClickableAreaPreview";
-export { ClickableAreaSettings } from "./ClickableAreaSettings";
-export type { ClickableArea, ClickableAreaData } from "./types";
